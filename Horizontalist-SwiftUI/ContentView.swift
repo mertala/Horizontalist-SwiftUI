38f34ae8c504b3a798dda80952eb1cad2bde0771
Mert <mertala@icloud.com>
2019-11-16 14:19:07 +0300

    Initial Commit

---
+++ Horizontalist-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Horizontalist-SwiftUI
+//
+//  Created by Mert Ala on 16.11.2019.
+//  Copyright © 2019 Mert Ala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
